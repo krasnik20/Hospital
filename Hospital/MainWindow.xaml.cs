c39156f6884e65ab46c77cfbb1fc6c3d1affe41b
Krasnikov <alesha.krasnikov@yandex.ru>
2021-05-16 02:53:01 +0300

    случайно закоммитил

--- Hospital/MainWindow.xaml.cs
+++ Hospital/MainWindow.xaml.cs
@@ -12,18 +12,18 @@ namespace Hospital
         public MainWindow()
         {
             InitializeComponent();
-            var serv = new PatientService();
-            var pat = new Patient { FirstName = "test", Diagnosis = new System.Collections.Generic.List<PatientDisease> { new PatientDisease { Disease = new Disease { Name = "Disease1" } }, new PatientDisease { Disease = new Disease { Name = "Disease2" } } } };
-            serv.Create(pat);
+            //var serv = new PatientService();
+            //var pat = new Patient { FirstName = "test", Diagnosis = new System.Collections.Generic.List<PatientDisease> { new PatientDisease { Disease = new Disease { Name = "Disease1" } }, new PatientDisease { Disease = new Disease { Name = "Disease2" } } } };
+            //serv.Create(pat);
 
-            var serv3 = new PatientService();
-            pat = serv3.Read()[4];
-            pat.Diagnosis = null;
-            pat.FirstName = "TEST";
-            serv3.Update(pat);
+            //var serv3 = new PatientService();
+            //pat = serv3.Read()[4];
+            //pat.Diagnosis = null;
+            //pat.FirstName = "TEST";
+            //serv3.Update(pat);
 
-            var serv2 = new PatientService();
-            var result = serv2.Read();
+            //var serv2 = new PatientService();
+            //var result = serv2.Read();
         }
     }
 }
