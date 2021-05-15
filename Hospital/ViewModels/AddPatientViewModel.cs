45cdfe3b2a40fb7274567442692703f887f920e9
Krasnikov <alesha.krasnikov@yandex.ru>
2021-05-15 22:50:51 +0300

    добавил базовую вью модель

---
+++ Hospital/ViewModels/AddPatientViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Model;
+using System;
+
+namespace Hospital.ViewModels
+{
+    class AddPatientViewModel : BaseViewModel
+    {
+        private Patient currentPatient;
+        private Disease[] availableDiseases;
+
+        public Patient CurrentPatient { get => currentPatient; set => SetProperty(ref currentPatient, value); }
+        public Disease[] AvailableDiseases { get => availableDiseases; set => SetProperty(ref availableDiseases, value); }
+
+        public AddPatientViewModel()
+        {
+        }
+
+    }
+}
---
+++ Hospital/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hospital.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        protected void SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "")
+        {
+            backingStore = value;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
