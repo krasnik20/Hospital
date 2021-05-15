ec9ecce3c33477ede725bd5c39ebb4320a6d8548
Krasnikov <alesha.krasnikov@yandex.ru>
2021-05-15 17:11:29 +0300

    initial commit

---
+++ Model/Patient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Diagnosis { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime DepartDate { get; set; }
+        public Room Room { get; set; }
+        public Cure Cure { get; set; }
+    }
+}
