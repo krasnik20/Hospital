9a9e90b5ac40f03ece29b8e9b07128764a3a387f
Diana <diane-v@mail.ru>
2021-05-16 14:57:27 +0300

    добавлена вью модель услуг

---
+++ Hospital/ViewModels/ServicesViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Hospital.Views.Dialogues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.ViewModels
+{
+    class ServicesViewModel : BaseViewModel
+    {
+        public RelayCommand AddServiceCommand { get; }
+
+        public ServicesViewModel()
+        {
+            AddServiceCommand = new RelayCommand(addService);
+        }
+
+        private void addService(object param)
+        {
+            var dialog = new AddServiceDialogue();
+            dialog.ShowDialog();
+        }
+    }
+}
